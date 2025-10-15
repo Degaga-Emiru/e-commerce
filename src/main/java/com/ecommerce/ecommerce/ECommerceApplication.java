15ef49c0b3bf726a7e67dfbcfebbce2fa9c7b1b6
Degaga Emiru <degagaemiru@gmail.com>
2025-10-15 21:33:30 +0300

    Add comment to main method in ECommerceApplication
    Added a comment to indicate the main method's purpose.

--- src/main/java/com/ecommerce/ecommerce/ECommerceApplication.java
+++ src/main/java/com/ecommerce/ecommerce/ECommerceApplication.java
@@ -1,7 +1,7 @@
 package com.ecommerce.ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//This is the Main Method where the application is launched
 @SpringBootApplication
 public class ECommerceApplication {
 
